e92ab16fec0e198fc38042d51954b1eaf53ea6e5
安 卢 <laa6203@163.com>
2017-12-20 14:49:20 +0800

    befor

--- main/fpga_s/src/syn_top/syn_top.v
+++ main/fpga_s/src/syn_top/syn_top.v
@@ -1,13 +1,16 @@
 //syn_top.v
 
 module syn_top(
-
+rx_syn,
+utc_sec,
+now_ns,
 //clk rst
 clk_sys,
 rst_n
 );
-
-
+input	rx_syn;
+output [31:0]	utc_sec;
+output [31:0]	now_ns;
 //clk rst
 input clk_sys;
 input rst_n;
