3842acafadb69856e7b809d8932150b15d4f7984
安 卢 <laa6203@163.com>
2018-01-06 17:37:44 +0800

    m commu_slot.v

--- main/fpga_s/src/commu_top/commu_slot.v
+++ main/fpga_s/src/commu_top/commu_slot.v
@@ -24,11 +24,113 @@ input rst_n;
 
 
 
+
+//---------- total slot and slot period ---------
+wire [7:0] total_slot;
+assign total_slot = (mode_numDev == 2'h3) ? 8'd25 :
+										(mode_numDev == 2'h1) ? 8'd20 : 
+										(mode_numDev == 2'h0) ? 8'd10 : 8'd25;
+wire [23:0] slot_period;
+`ifdef SIM
+assign slot_period =(mode_numDev == 2'h3) ? 24'd3_84 :
+										(mode_numDev == 2'h1) ? 24'd4_80 :
+										(mode_numDev == 2'h0) ? 24'd9_60 : 24'd3_84;
+`else 
+assign slot_period =(mode_numDev == 2'h3) ? 24'd39_950_00 :
+										(mode_numDev == 2'h1) ? 24'd49_940_00 :
+										(mode_numDev == 2'h0) ? 24'd99_880_00 : 24'd39_950_00;
+`endif
+
+
+//----------- main FSM ---------
+parameter S_IDLE = 3'h0;
+parameter S_HEAD = 3'h1;
+parameter S_MARK = 3'h2;
+parameter S_SLOT = 3'h3;
+parameter S_CHECK = 3'h4;
+parameter S_DONE = 3'h7;
+reg [2:0] st_commu_slot;
+wire finish_slot;
+wire finish_all;
+always @ (posedge clk_sys or negedge rst_n)	begin
+	if(~rst_n)
+		st_commu_slot <= S_IDLE;
+	else begin
+		case(st_commu_slot)
+			S_IDLE : st_commu_slot <= slot_begin ? S_HEAD : S_IDLE;
+			S_HEAD : st_commu_slot <= S_MARK;
+			S_MARK : st_commu_slot <= S_SLOT;
+			S_SLOT : st_commu_slot <= finish_slot ? S_CHECK : S_SLOT;
+			S_CHECK: st_commu_slot <= finish_all ? S_DONE : S_HEAD;
+			S_DONE : st_commu_slot <= S_IDLE;
+			default: st_commu_slot <= S_IDLE;
+		endcase
+	end
+end
+wire slot_head = (st_commu_slot == S_HEAD) ? 1'b1 : 1'b0;
+
+
+
+//---------- FSM finish_slot --------
+reg [23:0] cnt_slot_cycle;
+always @ (posedge clk_sys or negedge rst_n)	begin
+	if(~rst_n)
+		cnt_slot_cycle <= 24'h0;
+	else if( st_commu_slot == S_SLOT)
+		cnt_slot_cycle <= cnt_slot_cycle + 24'h1;
+	else 
+		cnt_slot_cycle <= 24'h0;
+end
+assign finish_slot = (cnt_slot_cycle == (slot_period - 24'h1)) ? 1'b1 : 1'b0;
+
+
+//------------- FMS finish_all --------
+reg [7:0] cnt_slot_id;
+always @ (posedge clk_sys or negedge rst_n)	begin
+	if(~rst_n)
+		cnt_slot_id <= 8'h0;
+	else if(st_commu_slot == S_HEAD)
+		cnt_slot_id <= cnt_slot_id + 8'h1;
+	else if(st_commu_slot == S_DONE)
+		cnt_slot_id <= 8'h0;
+	else ;
+end
+assign finish_all = (cnt_slot_id == total_slot) ? 1'b1 : 1'b0;
+
+
+//-------- slot_fix -----------
+wire slot_fix;
+assign slot_fix = (cnt_slot_id == dev_id) & (st_commu_slot == S_MARK);
+
+
+//--------- slot_cmd -----------
+wire slot_cmd;
 wire cmd_re;
-assign cmd_re = cmd_retry[0] & cmd_retry[1];
+assign cmd_re = cmd_retry[7];
+reg [7:0] cmd_slot_id;
+always @ (posedge clk_sys or negedge rst_n)	begin
+	if(~rst_n)
+		cmd_slot_id <= 8'h0;
+	else if(cmd_re)
+		cmd_slot_id = {1'b0,cmd_retry[6:0]};
+	else ;
+end
+reg lock_cmd_re;
+always @ (posedge clk_sys or negedge rst_n)	begin
+	if(~rst_n)
+		lock_cmd_re <= 1'b0;
+	else if(cmd_re)
+		lock_cmd_re <= 1'b1;
+	else if(finish_all)
+		lock_cmd_re <= 1'b0;
+	else ;
+end
+assign slot_cmd = lock_cmd_re & 
+									(cnt_slot_id == cmd_slot_id) & (st_commu_slot == S_MARK);
 
+//------------ slot_rdy ----------
+wire slot_rdy  = slot_fix | slot_cmd;
 
-wire slot_rdy  = 1'b0;
 
 endmodule
 
--- main/sim/tb_all/wave.do
+++ main/sim/tb_all/wave.do
@@ -1,10 +1,5 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
-add wave -noupdate /tb/top_s1/u_commu_top/u_commu_buf/cnt_buf
-add wave -noupdate /tb/top_s1/u_commu_top/u_commu_buf/pk_data
-add wave -noupdate /tb/top_s1/u_commu_top/u_commu_buf/pk_vld
-add wave -noupdate /tb/top_s1/u_commu_top/u_commu_buf/pk_frm
-add wave -noupdate /tb/top_s1/u_commu_top/u_commu_buf/len_pkg
 add wave -noupdate /tb/top_s1/u_commu_top/u_commu_main/st_commu_main
 add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/slot_begin
 add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/slot_rdy
@@ -12,8 +7,18 @@ add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/mode_numDev
 add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/dev_id
 add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/cmd_retry
 add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/cmd_re
+add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/st_commu_slot
+add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/finish_slot
+add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/finish_all
+add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/slot_head
+add wave -noupdate -radix unsigned /tb/top_s1/u_commu_top/u_commu_slot/cnt_slot_cycle
+add wave -noupdate -radix decimal /tb/top_s1/u_commu_top/u_commu_slot/cnt_slot_id
+add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/cmd_slot_id
+add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/lock_cmd_re
+add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/slot_fix
+add wave -noupdate /tb/top_s1/u_commu_top/u_commu_slot/slot_cmd
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {214494 ns} 0}
+WaveRestoreCursors {{Cursor 1} {161520 ns} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 309
 configure wave -valuecolwidth 100
@@ -29,4 +34,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ns
 update
-WaveRestoreZoom {207851 ns} {218409 ns}
+WaveRestoreZoom {0 ns} {644096 ns}
