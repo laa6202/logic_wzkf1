e9617741aac102f1efd4d49454279629bda72cfe
laa6202 <laa6203@163.com>
2018-01-11 08:57:17 +0800

    m arm_spi.v

--- main/sim/tb_all/dv/arm_spi.v
+++ main/sim/tb_all/dv/arm_spi.v
@@ -43,26 +43,26 @@ always @(posedge clk_sys or negedge rst_n)	begin
 	if(~rst_n)
 		cnt_spi_bit <= 4'b0;
 	else if(pluse_10M) begin
-		if(cnt_spi_bit == 4'h9)
+		if(cnt_spi_bit == 4'hc)
 			cnt_spi_bit <= 4'h0;
 		else 
 			cnt_spi_bit <= cnt_spi_bit + 4'h1;
 	end
 	else ;
 end
-wire spi_en = (cnt_spi_bit != 4'h8) & (cnt_spi_bit != 4'h9);
+wire spi_en = (cnt_spi_bit <= 4'h8) & (cnt_spi_bit >= 4'h1);
 
 reg spi_sck_i;
 always @ (posedge clk_sys or negedge rst_n)	begin
 	if(~rst_n)
-		spi_sck_i <= 1'b1;
-	else if(cnt_cycle == 4'h4)
 		spi_sck_i <= 1'b0;
-	else if(cnt_cycle == 4'h9)
+	else if(cnt_cycle == 4'h4)
 		spi_sck_i <= 1'b1;
+	else if(cnt_cycle == 4'h9)
+		spi_sck_i <= 1'b0;
 	else ;
 end
-wire spi_sck = spi_en ? spi_sck_i : 1'b1;
+wire spi_sck = spi_en ? spi_sck_i : 1'b0;
 
 
 //----------- spi_mosi -----------
