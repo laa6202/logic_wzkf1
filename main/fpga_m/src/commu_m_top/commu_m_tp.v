0d11890424f05bbb8f80c6e4c6709b069e8b1aed
laa6202 <laa6203@163.com>
2018-01-13 21:25:39 +0800

    a sof for spi_test

--- main/fpga_m/src/commu_m_top/commu_m_tp.v
+++ main/fpga_m/src/commu_m_top/commu_m_tp.v
@@ -19,19 +19,24 @@ input rst_n;
 //----------------------------------------
 //----------------------------------------
 
-reg [1:0] swift;
+reg [7:0] swift;
 always @ (posedge clk_sys or negedge rst_n)	begin
 	if(~rst_n)
-		swift <= 2'h0;
+		swift <= 8'h0;
 	else if(tp_rd)
-		swift <= swift + 2'h1;
+		swift <= swift + 8'h1;
 end
 
 wire [7:0]	tp_q;
+assign tp_q = swift;
+
+/*
 assign tp_q = (swift == 2'h0) ? 8'h55 : 
-							(swift == 2'h1) ? 8'h55 : 
-							(swift == 2'h2) ? 8'h55 : 
-							(swift == 2'h3) ? 8'h55 : 8'h55;
+							(swift == 2'h1) ? 8'hAA : 
+							(swift == 2'h2) ? 8'h5A : 
+							(swift == 2'h3) ? 8'hA5 : 8'h55;
+
+*/
 
 
 endmodule
