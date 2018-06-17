9dddb01c275975de57260d58901dd92042170823
laa6202 <laa6202@163.com>
2018-06-17 11:23:05 +0800

    add wd_rd_idle in commu_m_push.v

--- main/fpga_m/src/commu_m_top/commu_m_push.v
+++ main/fpga_m/src/commu_m_top/commu_m_push.v
@@ -37,6 +37,7 @@ wire buf_rd = real_rd;
 wire [7:0]	real_q = buf_q;
 
 reg [15:0] cnt_rd;
+wire wd_rd_idle;
 always @(posedge clk_sys or negedge rst_n)	begin
 	if(~rst_n)
 		cnt_rd <= 16'h0;
@@ -44,8 +45,24 @@ always @(posedge clk_sys or negedge rst_n)	begin
 		cnt_rd <= cnt_rd + 16'h1;
 	else if(cnt_rd == len_pkg)
 		cnt_rd <= 16'h0;
+	else if(wd_rd_idle)
+		cnt_rd <= 16'h0;
 	else ;
 end
+
+reg [29:0] cnt_rd_idle;
+always @ (posedge clk_sys or negedge rst_n)	begin	
+	if(~rst_n)
+		cnt_rd_idle <= 30'h0;
+	else if(real_rd)
+		cnt_rd_idle <= 30'h0;
+	else if(cnt_rd_idle == 30'h3fff_ffff)
+		cnt_rd_idle <= 30'h3fff_ffff;
+	else 
+		cnt_rd_idle <= cnt_rd_idle + 30'h1;
+end
+assign wd_rd_idle = (cnt_rd_idle == 30'd100_000_00) ? 1'b1 : 1'b0;
+
 
 wire buf_frm = (cnt_rd != 16'h0) | real_rd;
 
